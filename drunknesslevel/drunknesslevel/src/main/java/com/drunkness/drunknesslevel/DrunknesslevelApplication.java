e2f46653448c56d0fc137a205e332905235c1558
Achraf <achraf.zekri@uma.es>
2025-12-15 19:23:00 +0100

    Subida inicial del proyecto DrunknessLevel

---
+++ drunknesslevel/drunknesslevel/src/main/java/com/drunkness/drunknesslevel/DrunknesslevelApplication.java
@@ -0,0 +1,13 @@
+package com.drunkness.drunknesslevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrunknesslevelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrunknesslevelApplication.class, args);
+	}
+
+}
---
+++ drunknesslevel/drunknesslevel/src/test/java/com/drunkness/drunknesslevel/DrunknesslevelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drunkness.drunknesslevel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrunknesslevelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
